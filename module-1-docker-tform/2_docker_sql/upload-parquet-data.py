e93a6e21d5df5fddb5d8096d4ecb7e39f809fb91
Austinmc41 <austinmc41@gmail.com>
2025-01-27 18:33:37 -0500

    convert script to use argparse and work on hw

--- module-1-docker-tform/2_docker_sql/upload-parquet-data.py
+++ module-1-docker-tform/2_docker_sql/upload-parquet-data.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from time import time
 
@@ -6,22 +7,34 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
 
-def main(db_uri: str):
+def main(params):
 
-    input_file = f"/Users/austin/Desktop/dataeng-project/module-1-docker-tform/2_docker_sql/yellow_tripdata_2021-01.parquet"
-    parquet_file = pq.ParquetFile(input_file)
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    parquet_name = 'output.parquet'
+    os.system(f"wget {url} -O {parquet_name}")
+
+
+    # using dotenv
+    # uri = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+
+
+    parquet_file = pq.ParquetFile(parquet_name)
     parquet_size = parquet_file.metadata.num_rows
-
-    table_name = "yellow_taxi_schema"
-
-    engine = create_engine(uri)
 
     # default (and max) batch size
     index = 65536
 
     # handy way to infer and create schema without using DDL directly - grabs the structure from head
     # and creates table and drops it if it exists already
-    pq.read_table(input_file).to_pandas().head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    pq.read_table(parquet_name).to_pandas().head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
     for batch in parquet_file.iter_batches(use_threads=True):
         t_start = time()
@@ -34,17 +47,31 @@ def main(db_uri: str):
 
 if __name__ == '__main__':
 
-    load_dotenv()
+    # load_dotenv()
+
+    # POSTGRES_USER = os.getenv("POSTGRES_USER")
+    # POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    # POSTGRES_DB = os.getenv("POSTGRES_DB")
+    # POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    # POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+    parser.add_argument('--url', required=True, help='url of the csv file')
+
+    args = parser.parse_args()
+
+    # when using dotenv
+    # uri = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
-    POSTGRES_USER = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_DB = os.getenv("POSTGRES_DB")
-    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
-
-    uri = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    main(uri)
+    main(args)
 
 
 
